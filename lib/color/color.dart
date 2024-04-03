69f8bef415a72225b6bf3f57bd98c1c58551d684
AkariKokuba <b1023086@fun.ac.jp>
2024-04-03 15:34:17 +0900

    初めのページを作成

---
+++ lib/color/color.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AppColor {
+  static final _Brand brand = _Brand();
+  static final _Text text = _Text();
+  static final _Ui ui = _Ui();
+}
+
+class _Brand {}
+
+class _Text {}
+
+class _Ui {
+  final Color lightBlue = Color.fromARGB(188, 11, 97, 150);
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:study_app/color/color.dart';
 
 void main() {
   runApp(MyApp());
@@ -23,26 +24,90 @@ class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('hello'),
-          centerTitle: true,
-          backgroundColor: Colors.cyan,
-          leading: IconButton(
-            icon: Icon(Icons.menu),
-            onPressed: () {
-              /* setState((){
-
-              });*/
-            },
-          ),
-          actions: [Icon(Icons.add), Icon(Icons.done)],
-        ),
         body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Text("あああ"),
-            ],
+      child: IconButton(
+        icon: Icon(
+          Icons.cruelty_free,
+          size: 100,
+        ),
+        onPressed: () {
+          Navigator.of(context).push(MaterialPageRoute(builder: (context) {
+            return FirstPage();
+          }));
+        },
+      ),
+    ));
+  }
+}
+
+class FirstPage extends StatefulWidget {
+  const FirstPage({super.key});
+
+  @override
+  State<FirstPage> createState() => _FirstPage();
+}
+
+class _FirstPage extends State<FirstPage> {
+  List<String> strList = [
+    'テスト1',
+    'テスト2',
+    'テスト3',
+    'テスト4',
+    'テスト5',
+    'テスト6',
+    'テスト7',
+    'テスト8',
+    'テスト9',
+    'テスト10'
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          leading: IconButton(
+            onPressed: () {
+              Navigator.of(context).push(MaterialPageRoute(builder: (context) {
+                return MyHomePage(); //後で変える　今はmyhomepageに戻るようにしている
+              }));
+            },
+            icon: Icon(Icons.table_rows),
+          ),
+          title: Center(child: Text('StyleSnap')),
+          actions: [
+            IconButton(
+              onPressed: () {
+                Navigator.of(context)
+                    .push(MaterialPageRoute(builder: (context) {
+                  return MyHomePage(); //後で変える　今はmyhomepageに戻るようにしている
+                }));
+              },
+              icon: Icon(Icons.search),
+            ),
+          ],
+          backgroundColor: AppColor.ui.lightBlue, //Colors.はいらないらしい
+        ),
+        body: GridView.builder(
+          itemCount: strList.length,
+          itemBuilder: (context, index) {
+            return Padding(
+              padding: const EdgeInsets.only(top: 20, left: 20, right: 20),
+              child: Column(
+                children: [
+                  Container(
+                    color: Colors.blue,
+                    child: Center(child: Text(strList[index])),
+                  ),
+                  Container(child: Text(strList[index])),
+                ],
+              ),
+            );
+          },
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 3, //列の数を指定
+            mainAxisSpacing: 30,
+            crossAxisSpacing: 5, //グリッド間のスペースのサイズを指定
+            childAspectRatio: 0.9, //高さを指定
           ),
         ));
   }
